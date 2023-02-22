161e126461aeffe5f55db8322ca03ed58f59922a
mahmoud-ahmed-2002 <mahmoud.biltaji.2002@gmail.com>
2023-02-22 14:41:55 +0200

    add the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componet/home/home.component';
+import { AboutComponent } from './componet/about/about.component';
+import { FooterComponent } from './componet/footer/footer.component';
+import { NavbarComponent } from './componet/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
