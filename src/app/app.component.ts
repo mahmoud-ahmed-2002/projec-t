f29bcdea705539872401a20b1e776104f490dea1
mahmoud-ahmed-2002 <mahmoud.biltaji.2002@gmail.com>
2023-02-24 18:34:34 +0200

    add color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+
 
 @Component({
   selector: 'app-root',
