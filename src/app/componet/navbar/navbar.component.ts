eab7cb8d4690a631cce5c1926e46717cf2e57bef
mahmoud-ahmed-2002 <mahmoud.biltaji.2002@gmail.com>
2023-02-25 02:10:01 +0200

    end the navbar

--- src/app/componet/navbar/navbar.component.ts
+++ src/app/componet/navbar/navbar.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
 
+export class NavbarComponent {
+  isTest :boolean =false;
+  isDark :boolean =false;
 }
