ea6e14e34665bed0ef56253d2728c82387e16df2
mahmoud-ahmed-2002 <mahmoud.biltaji.2002@gmail.com>
2023-02-26 20:37:30 +0200

    end the navbar 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './componet/home/home.component';
 import { AboutComponent } from './componet/about/about.component';
 import { FooterComponent } from './componet/footer/footer.component';
 import { NavbarComponent } from './componet/navbar/navbar.component';
+import { CarouselComponent } from './componet/home/componet/carousel/carousel.component';
+
+
 
 @NgModule({
   declarations: [
@@ -14,11 +18,13 @@ import { NavbarComponent } from './componet/navbar/navbar.component';
     HomeComponent,
     AboutComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/componet/navbar/navbar.component.ts
+++ src/app/componet/navbar/navbar.component.ts
@@ -1,4 +1,10 @@
+
 import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
+
+
+declare var $: any;
+
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +12,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 
+
 export class NavbarComponent {
-  isTest :boolean =false;
-  isDark :boolean =false;
+  isTest: boolean = false;
+  isDark: boolean = false;
+  currentPosition = window.pageYOffset;
+
+  constructor() { }
+
+  //  ------------- navbar effect ---------
+  @HostListener('window:scroll', ['$event.target']) // for window scroll events
+  onWindowScroll() {
+    
+    let headerNav = document.querySelector('header') as HTMLElement;
+    let topNAv = document.querySelector('.top-nav') as HTMLElement;
+    let bottomNav = document.querySelector('.bottom-nav') as HTMLElement;
+
+    if (window.pageYOffset > headerNav.clientHeight) {
+      topNAv.classList.add('d-none');
+      bottomNav.classList.add('fixed-top');
+    } else {
+      topNAv.classList.remove('d-none');
+      bottomNav.classList.remove('fixed-top');
+    }
+    
+
+    // make header visible when scroll up and hid when scroll down
+    // let headerNav = document.querySelector("header");
+    // let lastScrollY = window.scrollY;
+
+    //   if (lastScrollY < window.scrollY) {
+    //     header.classList.add("headerHidden");
+    //     document.querySelector(".scrollInd").style.top = "0px";
+
+    //     //this is to toggle menu when i am scrolling up
+    //     //this is to toggle menu when i am scrolling up
+    //   } else {
+    //     header.classList.remove("headerHidden");
+    //     document.querySelector(".scrollInd").style.top = "74px";
+    //   }
+    //   lastScrollY = window.scrollY;
+  }
+  
+
+
+  ngOnInit(): void {
+
+    }
 }
